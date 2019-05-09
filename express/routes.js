b7ee8e9dd5f6149424b7763d205ef6a428b23d0f
Luis Perez <luis.perez.live@gmail.com>
2019-05-08 17:52:57 -0700

    Update packages, install other items

--- express/routes.js
+++ express/routes.js
@@ -1,8 +1,6 @@
 module.exports = function(app, angularPath, env) { 
     var game = require('./controllers/game')(env);
     app.get('/data/game/:name', game.findByName); 
-    app.get('/data/test', game.test);
-    app.get('/data/faction/:faction', game.getFactionData);
 
     app.get("*", function(req, res) {
         res.sendfile("index.html", { root: angularPath });
